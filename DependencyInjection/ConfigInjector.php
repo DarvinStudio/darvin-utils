4421655cd5f936bd28e566eda031f9322f0ae207
igor <igor.sv.n@gmail.com>
2015-08-20 17:30:46 +0300

    Implement config injector.

---
+++ DependencyInjection/ConfigInjector.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Config injector
+ */
+class ConfigInjector
+{
+    /**
+     * @param array                                                     $config    Config
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container DI container
+     * @param string                                                    $prefix    Parameter name prefix
+     */
+    public static function inject(array $config, ContainerInterface $container, $prefix)
+    {
+        foreach ($config as $name => $value) {
+            $name = $prefix.'.'.$name;
+            $container->setParameter($name, $value);
+
+            if (is_array($value)) {
+                self::inject($value, $container, $name);
+            }
+        }
+    }
+}
