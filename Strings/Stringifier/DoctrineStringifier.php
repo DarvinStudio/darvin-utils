7e14b8a6bb2c844bb90b95ee4781fd7be1e89ad0
igor <igor.sv.n@gmail.com>
2015-08-26 12:27:22 +0300

    Move stringifier to "darvinstudio/darvin-utils-bundle".

---
+++ Strings/Stringifier/DoctrineStringifier.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\Strings\Stringifier;
+
+use Doctrine\DBAL\Types\Type;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * Doctrine stringifier
+ */
+class DoctrineStringifier implements StringifierInterface
+{
+    /**
+     * @var array
+     */
+    private static $datetimeFormats = array(
+        Type::DATE       => 'd.m.Y',
+        Type::DATETIME   => 'd.m.Y H:i:s',
+        Type::DATETIMETZ => 'd.m.Y H:i:s',
+        Type::TIME       => 'H:i:s',
+    );
+
+    /**
+     * @var \Symfony\Component\Translation\TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @param \Symfony\Component\Translation\TranslatorInterface $translator Translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function stringify($value, $dataType)
+    {
+        switch ($dataType) {
+            case Type::BIGINT:
+            case Type::BLOB:
+            case Type::DECIMAL:
+            case Type::FLOAT:
+            case Type::GUID:
+            case Type::INTEGER:
+            case Type::SMALLINT:
+            case Type::STRING:
+            case Type::TEXT:
+                return $value;
+
+            case Type::BOOLEAN:
+                return $this->stringifyBoolean($value);
+
+            case Type::DATE:
+            case Type::DATETIME:
+            case Type::DATETIMETZ:
+            case Type::TIME:
+                return $this->stringifyDatetime($value, self::$datetimeFormats[$dataType]);
+
+            case Type::JSON_ARRAY:
+            case Type::SIMPLE_ARRAY:
+            case Type::TARRAY:
+                return $this->stringifyArray($value);
+
+            case Type::OBJECT:
+                return $this->stringifyObject($value);
+
+            default:
+                return '';
+        }
+    }
+
+    /**
+     * @param mixed $value Value to stringify
+     *
+     * @return string
+     */
+    private function stringifyArray($value)
+    {
+        return is_array($value) ? json_encode($value) : '';
+    }
+
+    /**
+     * @param mixed $value Value to stringify
+     *
+     * @return string
+     */
+    private function stringifyBoolean($value)
+    {
+        return $this->translator->trans(sprintf('boolean.%d', $value));
+    }
+
+    /**
+     * @param mixed  $value  Value to stringify
+     * @param string $format Datetime format
+     *
+     * @return string
+     */
+    private function stringifyDatetime($value, $format)
+    {
+        return $value instanceof \DateTime ? $value->format($format) : '';
+    }
+
+    /**
+     * @param mixed $value Value to stringify
+     *
+     * @return string
+     */
+    private function stringifyObject($value)
+    {
+        return is_object($value) ? serialize($value) : '';
+    }
+}
---
+++ Strings/Stringifier/StringifierInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\Strings\Stringifier;
+
+/**
+ * Stringifier
+ */
+interface StringifierInterface
+{
+    /**
+     * @param mixed  $value    Value to stringify
+     * @param string $dataType Data type
+     *
+     * @return string
+     */
+    public function stringify($value, $dataType);
+}
