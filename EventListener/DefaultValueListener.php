81416a6e2451c7e99c81b33c02baaf7358f3f5af
igor <igor.sv.n@gmail.com>
2015-09-01 10:39:50 +0300

    Replace page event listener with default value event listener.

---
+++ EventListener/DefaultValueListener.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\EventListener;
+
+use Darvin\Utils\DefaultValue\DefaultValueException;
+use Darvin\Utils\Mapping\MetadataFactoryProviderInterface;
+use Doctrine\Common\Util\ClassUtils;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+
+/**
+ * Default value event listener
+ */
+class DefaultValueListener extends AbstractOnFlushListener
+{
+    /**
+     * @var \Darvin\Utils\Mapping\MetadataFactoryProviderInterface
+     */
+    private $metadataFactoryProvider;
+
+    /**
+     * @var \Symfony\Component\PropertyAccess\PropertyAccessorInterface
+     */
+    private $propertyAccessor;
+
+    /**
+     * @param \Darvin\Utils\Mapping\MetadataFactoryProviderInterface      $metadataFactoryProvider Metadata factory provider
+     * @param \Symfony\Component\PropertyAccess\PropertyAccessorInterface $propertyAccessor        Property accessor
+     */
+    public function __construct(
+        MetadataFactoryProviderInterface $metadataFactoryProvider,
+        PropertyAccessorInterface $propertyAccessor
+    ) {
+        $this->metadataFactoryProvider = $metadataFactoryProvider;
+        $this->propertyAccessor = $propertyAccessor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onFlush(OnFlushEventArgs $args)
+    {
+        parent::onFlush($args);
+
+        foreach ($this->uow->getScheduledEntityInsertions() as $entity) {
+            $this->setDefaultValues($entity);
+        }
+        foreach ($this->uow->getScheduledEntityUpdates() as $entity) {
+            $this->setDefaultValues($entity);
+        }
+    }
+
+    /**
+     * @param object $entity Entity
+     *
+     * @throws \Darvin\Utils\DefaultValue\DefaultValueException
+     */
+    protected function setDefaultValues($entity)
+    {
+        $entityClass = ClassUtils::getClass($entity);
+
+        $meta = $this->metadataFactoryProvider->getMetadataFactory()->getMetadata($entityClass);
+        $defaultValuesMap = $meta['default_values'];
+        $this->filterDefaultValuesMap($defaultValuesMap, $entity, $entityClass);
+
+        if (empty($defaultValuesMap)) {
+            return;
+        }
+
+        $sourcePropertyValues = $this->getSourcePropertyValues(
+            array_unique(array_values($defaultValuesMap)),
+            $entity,
+            $entityClass
+        );
+
+        $recomputeChangeSet = false;
+
+        foreach ($defaultValuesMap as $targetProperty => $sourcePropertyPath) {
+            if (null === $sourcePropertyValues[$sourcePropertyPath]) {
+                continue;
+            }
+            if (!$this->propertyAccessor->isWritable($entity, $targetProperty)) {
+                throw new DefaultValueException(sprintf('Property "%s::$%s" is not writable.', $entityClass, $targetProperty));
+            }
+
+            $this->propertyAccessor->setValue($entity, $targetProperty, $sourcePropertyValues[$sourcePropertyPath]);
+
+            $recomputeChangeSet = true;
+        }
+        if ($recomputeChangeSet) {
+            $this->recomputeChangeSet($entity);
+        }
+    }
+
+    /**
+     * @param array  $defaultValuesMap Default values map to filter
+     * @param object $entity           Entity
+     * @param string $entityClass      Entity class
+     *
+     * @throws \Darvin\Utils\DefaultValue\DefaultValueException
+     */
+    private function filterDefaultValuesMap(array &$defaultValuesMap, $entity, $entityClass)
+    {
+        foreach ($defaultValuesMap as $targetProperty => $sourcePropertyPath) {
+            if (!$this->propertyAccessor->isReadable($entity, $targetProperty)) {
+                throw new DefaultValueException(sprintf('Property "%s::$%s" is not readable.', $entityClass, $targetProperty));
+            }
+
+            $value = $this->propertyAccessor->getValue($entity, $targetProperty);
+
+            if (null !== $value) {
+                unset($defaultValuesMap[$targetProperty]);
+            }
+        }
+    }
+
+    /**
+     * @param array  $sourcePropertyPaths Source property paths
+     * @param object $entity              Entity
+     * @param string $entityClass         Entity class
+     *
+     * @return array
+     * @throws \Darvin\Utils\DefaultValue\DefaultValueException
+     */
+    private function getSourcePropertyValues(array $sourcePropertyPaths, $entity, $entityClass)
+    {
+        $sourcePropertyValues = array();
+
+        foreach ($sourcePropertyPaths as $sourcePropertyPath) {
+            if (!$this->propertyAccessor->isReadable($entity, $sourcePropertyPath)) {
+                throw new DefaultValueException(
+                    sprintf('Property "%s::$%s" is not readable.', $entityClass, $sourcePropertyPath)
+                );
+            }
+
+            $sourcePropertyValues[$sourcePropertyPath] = $this->propertyAccessor->getValue($entity, $sourcePropertyPath);
+        }
+
+        return $sourcePropertyValues;
+    }
+}
