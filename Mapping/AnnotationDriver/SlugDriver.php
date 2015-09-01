8f2b70b9f67f4ad3c9b81a36f6194f6236cbead2
igor <igor.sv.n@gmail.com>
2015-09-01 09:45:14 +0300

    Replace sluggable interface with slug annotation.

---
+++ Mapping/AnnotationDriver/SlugDriver.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\Mapping\AnnotationDriver;
+
+use Darvin\Utils\Mapping\Annotation\Slug;
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+
+/**
+ * Slug annotation driver
+ */
+class SlugDriver extends AbstractDriver
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function readMetadata(ClassMetadata $doctrineMeta, array &$meta)
+    {
+        $meta['slugs'] = array();
+
+        foreach ($doctrineMeta->getReflectionClass()->getProperties() as $reflectionProperty) {
+            if (null !== $this->reader->getPropertyAnnotation($reflectionProperty, Slug::ANNOTATION)) {
+                $meta['slugs'][] = $reflectionProperty->getName();
+            }
+        }
+    }
+}
