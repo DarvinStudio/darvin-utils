ba3dbc15edf7602dbd5209a50b24f274f3ae78a9
Igor Nikolaev <igor.sv.n@gmail.com>
2018-10-01 09:05:57 +0300

    Add importable interface.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 6.5.0: New transliterator.
 
 6.5.4: Add "inverse" option to new object flag annotation.
+
+6.5.6: Add importable interface.
---
+++ Importable/ImportableInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2018, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\Importable;
+
+/**
+ * Importable
+ */
+interface ImportableInterface
+{
+    const IMPORT_ID_FIELD = 'importId';
+
+    /**
+     * @return string
+     */
+    public function getImportId();
+
+    /**
+     * @param string $importId Import ID
+     */
+    public function setImportId($importId);
+}
