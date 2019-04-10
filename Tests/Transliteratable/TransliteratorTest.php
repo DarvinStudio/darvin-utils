526c763281ac8a1fdeffd78866ec4fecea89d6b7
Igor Nikolaev <igor.sv.n@gmail.com>
2019-04-10 14:40:39 +0300

    Add transliterator test.

--- Tests/Transliteratable/TransliteratorTest.php
+++ Tests/Transliteratable/TransliteratorTest.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @author    Maxim Sukhanov <syhanov.m@yandex.ru>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Darvin\Utils\Tests\Transliteratable;
 
