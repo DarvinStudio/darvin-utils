388377cf1d62503c71d5e646ce161f985edf6845
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-05 09:13:10 +0300

    Enable strict types.

--- Routing/RouteCacheWarmer.php
+++ Routing/RouteCacheWarmer.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2017, Darvin Studio
+ * @copyright Copyright (c) 2017-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
@@ -33,7 +33,7 @@ class RouteCacheWarmer implements CacheWarmerInterface
     /**
      * {@inheritdoc}
      */
-    public function isOptional()
+    public function isOptional(): bool
     {
         return false;
     }
@@ -41,7 +41,7 @@ class RouteCacheWarmer implements CacheWarmerInterface
     /**
      * {@inheritdoc}
      */
-    public function warmUp($cacheDir)
+    public function warmUp($cacheDir): void
     {
         $this->cachedRouteManager->cacheRoutes();
     }
