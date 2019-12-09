10c884aa36575808d9b06c7bbc4fa703d0d3b5ea
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-09 09:37:17 +0300

    Test generic stringifier.

---
+++ Tests/Strings/Stringifier/StringifierTest.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\Tests\Strings\Stringifier;
+
+use Darvin\Utils\Strings\Stringifier\Stringifier;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Stringifier test
+ *
+ * @group strings
+ */
+class StringifierTest extends TestCase
+{
+    /**
+     * @var \Darvin\Utils\Strings\Stringifier\StringifierInterface
+     */
+    private $stringifier;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp(): void
+    {
+        $translator = $this->getMockBuilder(TranslatorInterface::class)->getMock();
+        $translator->method('trans')->willReturnArgument(0);
+
+        $this->stringifier = new Stringifier($translator);
+    }
+
+    public function testStringifyBoolean(): void
+    {
+        self::assertEquals('boolean.no', $this->stringifier->stringify(false));
+    }
+}
