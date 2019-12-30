2a1c62b9c51fe91880f3bbc43ad79dca30be5625
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-30 15:36:08 +0300

    Implement dummy entity overrider.

---
+++ Override/Overrider/EntityOverrider.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\Override\Overrider;
+
+use Darvin\Utils\Override\Config\Model\Subject;
+
+/**
+ * Entity overrider
+ */
+class EntityOverrider implements OverriderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function override(Subject $subject): void
+    {
+        dump($subject);
+    }
+}
