d5346c628013ae3f5ebff98d938fef0b5268756f
Igor Nikolaev <igor.sv.n@gmail.com>
2020-01-14 11:33:40 +0300

    Move price formatter from utils bundle to utils lib.

---
+++ Price/PriceFormatter.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2018-2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\Utils\Price;
+
+use Darvin\Utils\Service\ServiceProviderInterface;
+use Twig\Environment;
+
+/**
+ * Price formatter
+ */
+class PriceFormatter implements PriceFormatterInterface
+{
+    /**
+     * @var \Darvin\Utils\Service\ServiceProviderInterface
+     */
+    private $twigProvider;
+
+    /**
+     * @param \Darvin\Utils\Service\ServiceProviderInterface $twigProvider Twig service provider
+     */
+    public function __construct(ServiceProviderInterface $twigProvider)
+    {
+        $this->twigProvider = $twigProvider;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function format($price, array $options = []): string
+    {
+        if (!isset($options['format'])) {
+            $options['format'] = 'default';
+        }
+
+        return $this->getTwig()->render(isset($options['template']) ? $options['template'] : '@DarvinUtils/price.html.twig', [
+            'price'   => $price,
+            'options' => $options,
+        ]);
+    }
+
+    /**
+     * @return \Twig\Environment
+     */
+    final protected function getTwig(): Environment
+    {
+        return $this->twigProvider->getService();
+    }
+}
