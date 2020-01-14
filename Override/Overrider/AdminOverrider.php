1362ace8e4b818c26a7c7ca7ad1f4c5570bdcb01
Igor Nikolaev <igor.sv.n@gmail.com>
2020-01-14 10:38:20 +0300

    Implement admin overrider.

--- Override/Overrider/AdminOverrider.php
+++ Override/Overrider/AdminOverrider.php
@@ -12,12 +12,19 @@ namespace Darvin\Utils\Override\Overrider;
 
 use Darvin\AdminBundle\Configuration\SectionConfigurationInterface;
 use Darvin\Utils\Override\Config\Model\Subject;
+use Darvin\Utils\Strings\StringsUtil;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Admin section config overrider
  */
 class AdminOverrider implements OverriderInterface
 {
+    /**
+     * @var \Symfony\Component\Filesystem\Filesystem
+     */
+    private $filesystem;
+
     /**
      * @var \Darvin\AdminBundle\Configuration\SectionConfigurationInterface
      */
@@ -29,13 +36,26 @@ class AdminOverrider implements OverriderInterface
     private $bundlesMeta;
 
     /**
+     * @var string
+     */
+    private $projectDir;
+
+    /**
+     * @param \Symfony\Component\Filesystem\Filesystem                        $filesystem    Filesystem
      * @param \Darvin\AdminBundle\Configuration\SectionConfigurationInterface $sectionConfig Admin section configuration
      * @param array                                                           $bundlesMeta   Bundles metadata
+     * @param string                                                          $projectDir    Project directory
      */
-    public function __construct(SectionConfigurationInterface $sectionConfig, array $bundlesMeta)
-    {
+    public function __construct(
+        Filesystem $filesystem,
+        SectionConfigurationInterface $sectionConfig,
+        array $bundlesMeta,
+        string $projectDir
+    ) {
+        $this->filesystem = $filesystem;
         $this->sectionConfig = $sectionConfig;
         $this->bundlesMeta = $bundlesMeta;
+        $this->projectDir = $projectDir;
     }
 
     /**
@@ -47,15 +67,16 @@ class AdminOverrider implements OverriderInterface
             throw new \InvalidArgumentException(sprintf('Bundle "%s" does not exist.', $subject->getBundle()));
         }
         foreach ($subject->getEntities() as $entity) {
-            $this->overrideAdmin($entity, $this->bundlesMeta[$subject->getBundle()]['namespace']);
+            $this->overrideAdmin($entity, $this->bundlesMeta[$subject->getBundle()]['namespace'], $output);
         }
     }
 
     /**
-     * @param string $entity          Entity
-     * @param string $bundleNamespace Bundle namespace
+     * @param string        $entity          Entity
+     * @param string        $bundleNamespace Bundle namespace
+     * @param callable|null $output          Output callback
      */
-    private function overrideAdmin(string $entity, string $bundleNamespace): void
+    private function overrideAdmin(string $entity, string $bundleNamespace, ?callable $output): void
     {
         $fqcn = implode('\\', [$bundleNamespace, 'Entity', $entity]);
 
@@ -65,6 +86,47 @@ class AdminOverrider implements OverriderInterface
 
         $section = $this->sectionConfig->getSection($fqcn);
 
-        dump($section);
+        if (null === $section->getConfig() || 0 !== strpos($section->getConfig(), '@')) {
+            return;
+        }
+
+        $configBundle = $configPathname = null;
+
+        foreach (array_keys($this->bundlesMeta) as $bundleName) {
+            $prefix = sprintf('@%s/', $bundleName);
+
+            if (0 === strpos($section->getConfig(), $prefix)) {
+                $configBundle   = $bundleName;
+                $configPathname = substr_replace($section->getConfig(), '', 0, mb_strlen($prefix));
+
+                break;
+            }
+        }
+        if (null === $configBundle || null === $configPathname) {
+            return;
+        }
+
+        $parts = explode(DIRECTORY_SEPARATOR, $configPathname);
+        array_shift($parts);
+        array_shift($parts);
+
+        if (empty($parts)) {
+            return;
+        }
+
+        $targetContent  = sprintf('extends: \'%s\'', $section->getConfig());
+        $targetPathname = implode(
+            DIRECTORY_SEPARATOR,
+            array_merge(
+                [$this->projectDir, 'config', array_shift($parts), StringsUtil::toUnderscore(preg_replace('/Bundle$/', '', $configBundle))],
+                $parts
+            )
+        );
+
+        $this->filesystem->dumpFile($targetPathname, $targetContent);
+
+        if (null !== $output) {
+            $output($targetPathname);
+        }
     }
 }
